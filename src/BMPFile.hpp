5fafc49a8a5573308f87991e6f26fdd088c2c14d
xkevio <xkeviota@gmail.com>
2021-07-20 19:26:39 +0200

    Restructure folder hierarchy

---
+++ src/BMPFile.hpp
@@ -0,0 +1,30 @@
+#ifndef BMPFILE_HPP
+#define BMPFILE_HPP
+
+#include <fstream>
+#include <vector>
+
+#include "BMPHeader.hpp"
+
+class BMPFile {
+   private:
+    BMPFileHeader file_header;
+    BMPInfoHeader info_header;
+
+    std::int32_t width;
+    std::int32_t height;
+    std::int32_t bytes_per_pixel;
+
+    std::vector<std::uint8_t> pixel_table;
+    std::vector<std::uint8_t> grey_values;
+    void convert_to_grey();
+
+   public:
+    BMPFile(const std::string& file_name);
+    void convert_to_ascii(std::ofstream& file);
+
+    [[nodiscard]] std::int32_t get_width() const;
+    [[nodiscard]] std::int32_t get_height() const;
+    [[nodiscard]] std::int32_t get_bytes_per_pixel() const;
+};
+#endif
